ebecc7961325fe35fcfa1660162757f0613cdb37
Isabel Repetto <isargp@gmail.com>
2021-04-04 17:44:14 +0200

    till exercise 13 DONE

--- countries.md
+++ countries.md
@@ -24,3 +24,26 @@
 * Ejercicio 6: Agregar opción "population"
 
 * Ejercicio 7: Al elegir la opción population se obtendrá la población total del continente que el usuario haya indicado anteriormente, de no existir el archivo manejar el error.
+
+* Ejercicio 8: Luego de buscar un país e imprimirlo por pantalla preguntar si desea guardar la imagen del país encontrado.
+
+* Ejercicio 9: Descargar la imagen indicada con anterioridad en --> ./images (Nota: El formato de las imágenes de restcountries es SVG)
+
+* Ejercicio 10: Agregar una root function historial de búsqueda
+
+* Ejercicio 11: La función del ejercicio 10, entregará los nombres y las poblaciones de todos los países previamente buscados de la siguiente manera name:value**\n**poblacion: value
+
+* Ejercicio 12: Luego de mostrar la lista de países del ejercicio 10 preguntar si quiere descargar las banderas de los mismos
+
+* Ejercicio 13: Descargar todas las imágenes en un directorio aparte
+
+* Ejercicio 14: Crear una clase país attr--> name, capital, population
+
+* Ejercicio 15: Convertir TODOS los países en un objeto y luego podamos acceder a un atributo de clase y nos indique la población total
+
+MENU TIPO:
+--REST Countries--
+Country
+Region
+Population --> country||region --> if !country: req else: from json
+Search history --> lista de países --> Quiere descargar las imágenes de las banderas ?  write wb:pass
--- requests_exercises.py
+++ requests_exercises.py
@@ -25,10 +25,12 @@ def country_finder():
     name = input("\n¿Qué país desea buscar?: ")
     my_path = Path('./data/searched_countries.csv')
     
+    ##? MENSAJE ASÍNCRONO
     with ThreadPoolExecutor() as executor:
         future = executor.submit(get_data_from_ulr, area='country', to_search=name)
         print('\n\nObteniendo datos resultado.....')
-        
+    
+    ##? IMPRIMIR DATOS DEL PAIS SELECCIONADO Y GUARDARLOS EN searched_countries.csv    
     if future.result() != {"status": 404, "message": "Not Found"}:
         print(f"Name: {future.result()[0]['name']}\nCapital: {future.result()[0]['capital']}\nRegion: {future.result()[0]['region']}\nPopulation: {future.result()[0]['population']}\nArea: {future.result()[0]['area']}\nLanguage: {future.result()[0]['languages'][0]['name']}\nFlag url: {future.result()[0]['flag']}")
         data_to_save = [future.result()[0]['name'],future.result()[0]['capital'],future.result()[0]['region'],future.result()[0]['population'],future.result()[0]['area'],future.result()[0]['languages'][0]['name'],future.result()[0]['flag']]
@@ -37,9 +39,18 @@ def country_finder():
         else:
             head_line = ['name', 'capital', 'region', 'population', 'area', 'language', 'flag']
             add_to_csvFile(my_path,[head_line,data_to_save])
+        
+        ##? GUARDAR BANDERA EN FORMATO SVG
+        save_flag = input("¿Quieres guardar la bandera de este pais en formato svg? (y/n): ")
+        while not save_flag in "yYnN":
+            save_flag = input("\n\nRESPUESTA INCORRECTA\n¿Quieres guardar la bandera de este pais en formato svg? (y/n): ")
+        if save_flag in "yY":
+            response = req.get(future.result()[0]['flag'])
+            save_flag_svg('./images',response.content, name)
+        
     else:
         print(f"\nNo hay resultados para la búsqueda que estás realizando.")
-  
+          
 def continent_countries_extraction():
     region = input("\n¿Qué continente desea buscar?: ")
     my_path = Path(f'./data/{region}.json')
@@ -54,6 +65,10 @@ def continent_countries_extraction():
     else:
         print(f"\nNo hay resultados para la búsqueda que estás realizando.")
         
+def save_flag_svg(path, img_content, name):
+    with open(path+f"/{name}.svg", "wb") as img:
+        img.write(img_content)
+
 def get_total_population():
     continent = input('\nContinente del que desea saber la población total: ')
     try:
@@ -67,30 +82,51 @@ def get_total_population():
         print('\nERROR: No existe el fichero del continente del que desea obtener la población total.\nRealiza primero la búsqueda del continente y consulta la población de nuevo.\nQuizás si existe, pero simplemente escribiste mal el continente.')
         return None
 
+def backup_history_flags(flags_container):
+    for url in flags_container[1:-1]:
+        url_split = url.split('/')
+        file_name = url_split[-1].split('.')
+        name = file_name[0]
+        response = req.get(url)    
+        save_flag_svg('./flags_history_backup', response.content, name)
+
+def search_history():
+    flags_container = []
+    with open('./data/searched_countries.csv', 'r') as file:
+        lectura = csv.reader(file)
+        for row in lectura:
+            print(f"name:{row[0]}**\n**poblacion: {row[3]}")
+            flags_container.append(row[-1])
+    save_flags_svg = input("¿Desea guardar todas las banderas correspondientes a los países del historial de búsqueda? (y/n): ") 
+    while not save_flags_svg in "yYnN":
+        save_flags_svg = input("\n\nRESPUESTA INCORRECTA\n¿Desea guardar todas las banderas correspondientes a los países del historial de búsqueda? (y/n): ")
+    if save_flags_svg in 'yY':
+        backup_history_flags(flags_container)
+
 def menu():
     os.system('cls')
-    opt=input('\nMENÚ PRINCIPAL\n1. Pais\n2. Continente\n3. Población\n4. Salir\n\nOPCIÓN: ')
-    while not opt in ('1','2','3','4'):
-        opt=input('\nOPCION INCORRECTA\n1. Pais\n2. Continente\n3. Población\n3. Salir\n\nOPCIÓN: ')
-    return opt
+    opt=input('\nMENÚ PRINCIPAL\n1. Pais\n2. Continente\n3. Población\n4. Historial de búsqueda\n5. Salir\n\nOPCIÓN: ')
+    while not opt in '12345':
+        opt=input('\nOPCION INCORRECTA\n1. Pais\n2. Continente\n3. Población\n4. Historial de búsqueda\n5. Salir\n\nOPCIÓN: ')
+
+    if opt == '1':
+        country_finder()
+    elif opt == '2':
+        continent_countries_extraction()
+    elif opt == '3':
+        continent_total_population = get_total_population()
+        if continent_total_population != None:
+            print(f"{continent_total_population['continent']}: {continent_total_population['population']}")
+    elif opt == '4':
+        search_history()
+    else:
+        print('Hasta pronto...')
+        sys.exit()
 
 def main():
    
     while True:
-        opt = menu()
-        if opt == '1':
-            country_finder()
-        elif opt == '2':
-            continent_countries_extraction()
-        elif opt == '3':
-            continent_total_population = get_total_population()
-            if continent_total_population != None:
-                print(f"{continent_total_population['continent']}: {continent_total_population['population']}")
-        else:
-            print('Hasta pronto...')
-            break
+        menu()
         input('\nPulse enter para continuar')
-         
+     
 main()
-
-sys.exit()
