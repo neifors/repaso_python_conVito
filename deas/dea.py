d6347a1d87c357216b2ce839b45465528e561cc8
Isabel Repetto <isargp@gmail.com>
2021-04-27 11:43:35 +0200

    deas last update

--- deas/dea.py
+++ deas/dea.py
@@ -1,11 +1,16 @@
-
-
+from math import sqrt
 class Dea():
     
-    def __init__(self, x, y, id_code):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, id_code, address):
+        self.x = float(x)
+        self.y = float(y)
         self.id = id_code
+        self.address = address
         
-    # def get_distance(self):
+    
+    def get_distance(self,x_user,y_user):
+        leg1 = abs(x_user - self.x)
+        leg2 = abs(y_user - self.y)
+        distance = sqrt(leg1**2 + leg2**2)
+        return distance
         
--- deas/main.py
+++ deas/main.py
@@ -47,6 +47,49 @@ def create_user():
     password = input("\nPassword: ")
     return User(name, password)
 
+def create_dea(deafile):
+    address = deafile["direccion_via_codigo"]+' '+deafile["direccion_via_nombre"]+' '+deafile["direccion_portal_numero"]+'\n'+deafile["direccion_ubicacion"]+'\n'+deafile["direccion_codigo_postal"]
+    return Dea(deafile["direccion_coordenada_x"],deafile["direccion_coordenada_y"],deafile["codigo_dea"], address)
+
+def to_access():
+    name = input("\nNombre: ")
+    password = input("\nPassword: ")
+    users = downdload_users_backup()
+    if users != None:
+        for user in users["data"]:
+            if user["name"] == name and user["password"] == password:
+                return True
+        print("\nEl usuario y la contraseña no coinciden.")
+        return False
+    
+def deas_menu():
+    options = ["1 - Buscar DEA por código","2 - Buscar DEA por distancia","3 - Atras"]
+    [print(option) for option in options]
+    opt = input("\nOpcion: ")
+    while not opt in ("123"):
+        opt = input("\nOpcion incorrecta. Vuelva a introducir una opcion (1,2 o 3): ")
+    return opt
+
+def dea_by_code(datafile):
+    code = input("\nCodigo del DEA: ")
+    for dea in datafile:
+        if dea["codigo_dea"] == code:
+            return dea 
+    return None
+
+def dea_by_distance(datafile):
+    x_user = float(input("\nSu posición x: "))
+    y_user = float(input("Su posición y: "))
+    dea = None
+    distance = 100000
+    for deafile in datafile:
+        aux_dea = create_dea(deafile)
+        aux_distance = aux_dea.get_distance(x_user, y_user)
+        if aux_distance < distance: 
+            distance = aux_distance
+            dea = deafile
+    return dea
+
 def menu():
     options = ["1 - Crear usuario","2 - Acceder","3 - Admins","4 - Salir"]
     [print(option) for option in options]
@@ -59,25 +102,47 @@ def main():
     
     data = get_deas_data()  
     users = downdload_users_backup() 
+    os.system('cls')
     opt = menu()
     
     while opt != "4": 
-            
+        #? NEW USER    
         if opt == "1":
             user = create_user()
             if users != None:
                 users["data"].append({"name":user.name,"password":user.password})
             else:
                 users = {"data":[{"name":user.name,"password":user.password}]}
-        # print(f"\nNº total de DEAS en la comunidad de Madrid: {n_deas(data)}")
+        #? TO ACCES 
+        if opt == "2":
+            cont = 1
+            permission = to_access()
+            while not permission and not cont == 3:
+                print(f"Quedan {3-cont} intentos.")
+                cont += 1
+                permission = to_access()
+            #? PERMITED USER
+            if permission:
+                os.system("cls")
+                opt = deas_menu()
+                #? DEA BY CODE
+                if opt == "1":
+                    deafile = dea_by_code(data)
+                    print(create_dea(deafile).address) if deafile != None else print("\nDEA no encontrado")
+                #? DEA BY DISTANCE    
+                elif opt == "2":
+                    nearest_dea = dea_by_distance(data)
+                    print(nearest_dea)
                 
+                
+        #?Pruebas con el json de deas
+        # print(f"\nNº total de DEAS en la comunidad de Madrid: {n_deas(data)}")            
         # deas_inside_m30 = filter(lambda dea: dea != None, list(sample_by_postalcodes(postalcodes_inside_m30,data)))
         # print(f"\nNº total de DEAS dentro de la M-30: {n_deas(list(deas_inside_m30))}")
-
         # deas_by_ownership = list(samples_by_ownership(data)) #lista de listas. La primera son los deas privados y la segunda los deas publicos
         # print(f"\n{deas_by_ownership[0][0]['tipo_titularidad']}: {len(deas_by_ownership[0])}\n{deas_by_ownership[1][0]['tipo_titularidad']}: {len(deas_by_ownership[1])}")
 
-        input("Pulsa ENTER para continuar")  
+        input("\nPulsa ENTER para continuar")  
         os.system('cls')
         opt = menu()
         
